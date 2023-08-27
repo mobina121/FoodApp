13340f9329cf6784f320890e8b0dd11e05e7a68d
zeinab.jalali <zeinab.jalali@gmail.com>
2023-08-27 22:57:52 +0330

    add navigation formation

---
+++ app/src/main/java/com/examplepart/foodpart/ui/screens/food/FullscreenImageScreen.kt
@@ -0,0 +1,11 @@
+package com.examplepart.foodpart.ui.screens.food
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun FullscreenImageScreen() {
+    Text("Fullscreen Image Screen", fontSize = 45.sp)
+}
