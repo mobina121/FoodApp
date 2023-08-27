4d9563cd85f187d3bb167396d63dfcc46b8d7cd1
zeinab.jalali <zeinab.jalali@gmail.com>
2023-08-27 19:47:43 +0000

    Revert "Merge branch '1-screen-navigation' into 'master'"
    This reverts merge request !1

--- app/src/main/java/com/examplepart/foodpart/MainActivity.kt
+++ app/src/main/java/com/examplepart/foodpart/MainActivity.kt
@@ -3,19 +3,11 @@ package com.examplepart.foodpart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.examplepart.foodpart.ui.core.AppScreens
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = AppScreens.Categories.route) {
-                mainNavGraph()
-            }
         }
     }
-
 }
