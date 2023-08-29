715f2fb7cedde65460086044183a07f6619bd212
mobina121 <zr.evergreen2020@gmail.com>
2023-08-29 13:24:48 +0330

    feature: splash screen done.

--- app/src/main/java/com/examplepart/foodpart/MainActivity.kt
+++ app/src/main/java/com/examplepart/foodpart/MainActivity.kt
@@ -3,11 +3,15 @@ package com.examplepart.foodpart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
+            Text("Hiii")
         }
     }
 }
