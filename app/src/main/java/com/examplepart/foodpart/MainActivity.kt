6b19b7b3897e9e46e296d3b93fb06b4d63770a4f
zeinab.jalali <zeinab.jalali@gmail.com>
2023-08-30 15:57:42 +0330

    add more navigation files

--- app/src/main/java/com/examplepart/foodpart/MainActivity.kt
+++ app/src/main/java/com/examplepart/foodpart/MainActivity.kt
@@ -3,6 +3,12 @@ package com.examplepart.foodpart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.examplepart.foodpart.ui.core.AppScreens
@@ -13,9 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = AppScreens.Categories.route) {
-                mainNavGraph()
+                mainNavGraph(navController)
             }
         }
     }
-
 }
--- app/src/main/java/com/examplepart/foodpart/ui/screens/whatotcook/WhatToCookScreen.kt
+++ app/src/main/java/com/examplepart/foodpart/ui/screens/whatotcook/WhatToCookScreen.kt
@@ -1,10 +1,41 @@
 package com.examplepart.foodpart.ui.screens.whatotcook
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.examplepart.foodpart.ui.core.AppScreens
 
 @Composable
-fun WhatToCookScreen() {
-    Text(text = "What To Cook Screen", fontSize = 45.sp)
+fun WhatToCookScreen(navController: NavController) {
+    Column {
+        Text(text = "What To Cook Screen", fontSize = 45.sp)
+        Row {
+            Button(onClick = {
+                navController.navigate(AppScreens.Categories.route)
+            }, Modifier.weight(1f)) {
+                Text("Navigate Categories Screen", fontSize = 20.sp)
+            }
+            Button(onClick = {
+                navController.navigate(AppScreens.Profile.route)
+            }, Modifier.weight(1f)) {
+                Text("Navigate Profile Screen", fontSize = 20.sp)
+            }
+            Button(
+                onClick = { navController.navigate(AppScreens.Search.route) },
+                Modifier.weight(1f)
+            ) {
+                Text("Navigate Search Screen", fontSize = 20.sp)
+            }
+        }
+        Button(onClick = {
+            navController.navigate(AppScreens.WhatToCookResult.route)
+        }) {
+            Text(text = "Navigate What To Cook Result Screen", fontSize = 20.sp)
+        }
+    }
 }
