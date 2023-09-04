38e8b14b4fe1a465cd6e51e88e138cc65b91748a
mobina121 <zr.evergreen2020@gmail.com>
2023-09-04 10:28:17 +0330

    edit: change navigation version

--- app/src/main/java/com/examplepart/foodpart/MainActivity.kt
+++ app/src/main/java/com/examplepart/foodpart/MainActivity.kt
@@ -3,15 +3,7 @@ package com.examplepart.foodpart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.examplepart.foodpart.ui.core.AppScreens
