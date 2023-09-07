44327fce8c3fc70c5d91be056dd3721c91a0ac52
mobina121 <zr.evergreen2020@gmail.com>
2023-09-07 21:58:51 +0330

    init: initial commit

--- app/src/main/java/com/examplepart/foodpart/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/examplepart/foodpart/ui/screens/profile/ProfileScreen.kt
@@ -1,11 +1,61 @@
 package com.examplepart.foodpart.ui.screens.profile
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarHost
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.examplepart.foodpart.R
+import com.examplepart.foodpart.ui.common.FoodPartAppBar
 
 @Composable
-fun ProfileScreen() {
-    Text("Profile Screen", fontSize = 45.sp)
+fun ProfileScreen(navController: NavController) {
+    ProfileScreenContent()
+}
+
+@Composable
+private fun ProfileScreenContent(
+) {
+
+    Scaffold(
+        topBar = {
+            FoodPartAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp, 16.dp, 16.dp, 0.dp),
+                title = stringResource(id = R.string.profile),
+                showStartIcon = false,
+                showEndIcon = false,
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(it) { data ->
+                Snackbar(
+                    backgroundColor = Color.DarkGray,
+                    contentColor = MaterialTheme.colors.onBackground,
+                    snackbarData = data
+                )
+            }
+        }
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(horizontal = 16.dp),
+
+            ) {
+        }
+    }
+
 }
