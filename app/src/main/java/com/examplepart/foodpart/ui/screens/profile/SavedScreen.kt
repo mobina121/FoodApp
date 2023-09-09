36b17248e555ba34137e6d57980d98c9904cb322
zeinab.jalali <zeinab.jalali@gmail.com>
2023-09-09 23:21:19 +0330

    add SavedScreen.kt

---
+++ app/src/main/java/com/examplepart/foodpart/ui/screens/profile/SavedScreen.kt
@@ -0,0 +1,11 @@
+package com.examplepart.foodpart.ui.screens.profile
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun SavedScreen() {
+    Text("Saved Screen", fontSize = 45.sp)
+}
